09939a58aa20632bef35b436cff26d5e48ac1c75
Uriya-hadad <80356969+Uriya-hadad@users.noreply.github.com>
2025-05-06 21:20:41 +0300

    Update autoUpdate.sh

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -19,7 +19,7 @@ trap 'echo "removing lock";rm -f "$LOCKFILE"; exit' EXIT
 # Configuration
 BASE_URL="https://common.repositories.cloud.sap/artifactory/portal/go/plugins/goli/"
 USERNAME="i564168"
-PASSWORD="cmVmdGtuOjAxOjE3NTUwMDU0NjE6bG11dngyWEZINEV5QmVrZUhYZ2k2NHZ6Q3NK"
+PASSWORD="****************"
 VERSION_FILE="version.txt"
 LATEST_VERSION_FILE="latest_version.txt"
 OUTPUT_FILE="Goli-latest-version.zip"
